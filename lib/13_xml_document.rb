bb660f118c083bb4cb64adb0bdc41d4491219cc6
Jeffrey Liu <mangomystery@gmail.com>
2016-09-30 22:22:27 -0700

    exercise 13

--- lib/13_xml_document.rb
+++ lib/13_xml_document.rb
@@ -1,3 +1,32 @@
 class XmlDocument
-  # TODO: your code goes here!
+  def initialize(indent = false)
+    @indent = indent
+    @indent_level = 0
+  end
+
+  def method_missing(method_name, options = {}, &block)
+    string = ""
+    string << "<#{method_name}"
+
+    options.each do |key, value|
+      string << " #{key}=\"#{value}\""
+    end
+
+    if block
+      string << ">"
+      string << "\n" if @indent
+      @indent_level += 1
+      string << ("  " * @indent_level) if @indent
+      string << yield
+      @indent_level -= 1
+      string << ("  " * @indent_level) if @indent
+      string << "</#{method_name}>"
+      string << "\n" if @indent
+    else
+      string << "/>"
+      string << "\n" if @indent
+    end
+
+    string
+  end
 end
